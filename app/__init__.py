4aca688c51e7e26393b0a88f51464bd52f690f57
Tobias Becher <25723425+TB-DevAcc@users.noreply.github.com>
2020-06-01 15:55:55 +0700

    removed redundant external folder

--- app/__init__.py
+++ app/__init__.py
@@ -22,7 +22,7 @@ def register_extensions(app):
 
 
 def register_blueprints(app):
-    for module_name in ("base", "home", "external"):
+    for module_name in ("base", "home"):
         module = import_module("app.{}.routes".format(module_name))
         app.register_blueprint(module.blueprint)
 
