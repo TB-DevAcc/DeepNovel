dd0965d061ab7b180f1565b9c271b0bca0d9068b
Tobias Becher <25723425+TB-DevAcc@users.noreply.github.com>
2020-06-01 18:46:04 +0700

    added pipelines directly

---
+++ app/base/pipes.py
@@ -0,0 +1,62 @@
+from transformers import pipeline
+
+# Ready to Use Pipelines for inference
+pipes = {
+    "qa": pipeline(
+        "question-answering"
+    ),  # print(nlp(question="What is extractive question answering?", context=context))
+    "fm": pipeline(
+        "fill-mask"
+    ),  # print(nlp(f"HuggingFace is creating a {nlp.tokenizer.mask_token} that the community uses."))
+    "tg": pipeline(
+        "text-generation"
+    ),  # print(text_generator("As far as I am concerned, I will", max_length=50))
+    "ne": pipeline("ner"),  # print(nlp(sequence))
+    # "sm": pipeline("summarization"),  # print(summarizer(ARTICLE, max_length=130, min_length=30))
+    "tr": pipeline(
+        "translation_en_to_de"
+    ),  # print(translator("Hugging Face is a technology company based in New York and Paris", max_length=40))
+}
+
+
+class AI:
+    def __init__(
+        self, pipes=pipes,
+    ):
+        self.pipes = pipes
+
+    def generate(self, doc, max_length=5000, min_length=0):
+        """
+        Generates text
+        """
+        return self.pipes["tg"](doc, max_length=max_length, min_length=min_length)
+
+    def mark_entities(self, doc):
+        """
+        marks entities in document
+        """
+        return self.pipes["ne"](doc)
+
+    def summarize(self, doc, max_length=50, min_length=10):
+        """
+        summarizes a document
+        """
+        return self.pipes["sm"](doc, max_length=max_length, min_length=min_length)
+
+    def translate(self, doc, from_lang="en", to_lang="de"):
+        """
+        translates a document from from_lang to to_lang
+        """
+        return self.pipes["tr"](doc)
+
+    def answer(self, question, doc):
+        """
+        answers a question in the context of the document
+        """
+        return self.pipes["qa"](question, context=doc)
+
+    def fill(self, doc):
+        """
+        fills in masked tokens in document
+        """
+        return self.pipes["fm"](doc)
--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 """
 MIT License
-Copyright (c) 2019 - present AppSeed.us
+Copyright (c) 2020 - Tobias Becher
 """
 
 from os import environ
@@ -13,14 +13,12 @@ from flask_migrate import Migrate
 from app import create_app, db
 from config import config_dict
 
-# WARNING: Don't run with debug turned on in production!
+# CHANGE FOR PRODUCTION
 DEBUG = config("DEBUG", default=True)
 
-# The configuration
 get_config_mode = "Debug" if DEBUG else "Production"
 
 try:
-    # Load the configuration using the default values
     app_config = config_dict[get_config_mode.capitalize()]
 
 except KeyError:
